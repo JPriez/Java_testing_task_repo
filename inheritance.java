b79818ddd6a2d154e3baec5b8229f3a6b80389bf
Oleg Kononenko <kononenko@active-bridge.com>
2016-07-24 12:26:28 +0300

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,26 @@
+public class User {
+    int age;//field
+    String firstName;//field
+    String lastName;//field
+
+    public User(int age, String firstName, String lastName) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
+
+
+public class Student extends User{
+    int group;
+
+    public Student(int age, String firstName, String lastName, int group) {
+        super(age, firstName, lastName);
+        this.group = group;
+    }
+
+    boolean isMyGroup(int g){
+        return g==group;
+    }
+}
