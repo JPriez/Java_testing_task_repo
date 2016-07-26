8cb6d29ccf233a4f9f556dc488f7f27be469e03e
Oleg Kononenko <kononenko@active-bridge.com>
2016-07-26 15:30:39 +0300

    Update polymorphism_mark2.java
    In this example, we pass two different types of objects to the method searchOn , 
    and inside searchOn objects of different types are processed uniformly and performed various activities. 
    It was an example of dynamic polymorphism.

--- polymorphism_mark2.java
+++ polymorphism_mark2.java
@@ -1,1 +1,26 @@
-
+interface SearchEngine {
+    void search();
+}
+class GoogleEngine implements SearchEngine {
+    @Override
+    public void search() {
+        System.out.println("search on google");
+    }
+}
+class YandexEngine implements SearchEngine {
+    @Override
+    public void search() {
+        System.out.println("search on yandex");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        GoogleEngine google = new GoogleEngine();
+        YandexEngine yandex = new YandexEngine();
+        searchOn(google);   // print "search on google"
+        searchOn(yandex);   // print "search on yandex"
+    }   
+    private static void searchOn(SearchEngine engine) {
+        engine.search();       
+    }
+}
