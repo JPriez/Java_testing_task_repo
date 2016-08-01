046984e203c7aeb07bbdca560ecb9c1ef8ee8770
Oleg Kononenko <kononenko@active-bridge.com>
2016-08-01 13:42:14 +0300

    Update encapsulation_mark3.java
    We need to pass values in the control class using private variables and to return only the values obtained after processing the protected class to the main program. However, none of the class or method does not see what is in a protected class and could not turn to his methods, but the class can use the methods of public classes and variables declared as public

--- encapsulation_mark3.java
+++ encapsulation_mark3.java
@@ -1,1 +1,66 @@
+public class Robot {
+	// Current X coordinate
+    private double x = 0;
+    // Current Y coordinate
+    private double y = 0;
+    // The current rate (in degrees)
+    private double course = 0;
+ 
+    // Moving at a distance of "distance"
+    public void forward(int distance) {
+        // Appeal to the field object X
+        x = x + distance * Math.cos(course / 180 * Math.PI);
+        // Appeal to the field object Y
+        y = y + distance * Math.sin(course / 180 * Math.PI);
+    }
+ 
+    // Print robot's coordinates
+    public void printCoordinates() {
+        System.out.println(x + "," + y);
+    }
+ 
+    public double getX() {
+        return x;
+    }
+ 
+    public double getY() {
+        return y;
+    }
+ 
+    public double getCourse() {
+        return course;
+    }
+ 
+    public void setCourse(double course) {
+        this.course = course;
+    }
+}
 
+public class RobotManager {
+	public static void main(String[] args) {
+        // Create an object of class Robot
+        Robot robot = new Robot();
+ 
+        // Forward for 20 meters
+        robot.forward(20);
+        // Print coordinates
+        robot.printCoordinates();
+ 
+        // This is a more correct way to change course.
+        // Implementation within the robot is not much different, but
+        // we can at any time make a more advanced version
+        // But Robot Manager class do not even know this
+        robot.setCourse(90);
+        // Forward for 20 meters
+        robot.forward(20);
+        // Print coordinates
+        robot.printCoordinates();
+ 
+        // The course is 45 degrees
+        robot.setCourse(45);
+        // Forward for 20 meters
+        robot.forward(20);
+        // Print coordinates
+        robot.printCoordinates();
+    }
+}
